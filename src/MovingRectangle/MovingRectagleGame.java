2522995c67253a704677940d6b514076e07099e5
LizaElena <2234743@cegepst.qc.ca>
2024-09-18 14:09:01 -0400

    feature: amazing new feature

--- src/MovingRectangle/MovingRectagleGame.java
+++ src/MovingRectangle/MovingRectagleGame.java
@@ -3,20 +3,27 @@ package MovingRectangle;
 import Doctrina.Canvas;
 import Doctrina.Game;
 
-public class MovingRectagleGame extends Game {
+import java.awt.*;
 
+public class MovingRectagleGame extends Game {
+    private Player player;
+    private Npc npc;
     @Override
     protected void initialize() {
-
+        player = new Player();
+        npc = new Npc();
     }
 
     @Override
     protected void update() {
-
+        player.update();
+        npc.update();
     }
 
     @Override
     protected void draw(Canvas canvas) {
-
+        canvas.drawRectagle(0,0,800,600, Color.BLUE);
+        player.draw(canvas);
+        npc.draw(canvas);
     }
 }
---
+++ src/MovingRectangle/Player.java
@@ -0,0 +1,25 @@
+package MovingRectangle;
+
+import Doctrina.Canvas;
+
+import java.awt.*;
+
+public class Player {
+    private int x;
+    private int y;
+    private int speed;
+    public Player(){
+        x = 200;
+        y = 200;
+        speed = 3;
+    }
+
+    public void update(){
+        x += speed;
+    }
+
+    public void draw(Canvas canvas){
+        canvas.drawRectagle(x,y,20,60, Color.WHITE);
+    }
+
+}
