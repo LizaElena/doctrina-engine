3b1d41b64b8736098d02e1393cba2b2c593fae4a
LizaElena <2234743@cegepst.qc.ca>
2024-09-18 13:48:41 -0400

    feature: amazing new game

---
+++ src/MovingRectangle/MovingRectagleGame.java
@@ -0,0 +1,22 @@
+package MovingRectangle;
+
+import Doctrina.Canvas;
+import Doctrina.Game;
+
+public class MovingRectagleGame extends Game {
+
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    protected void update() {
+
+    }
+
+    @Override
+    protected void draw(Canvas canvas) {
+
+    }
+}
