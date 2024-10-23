fdabf847af799f9d45e0cf1b9971491c57be0983
LizaElena <2234743@cegepst.qc.ca>
2024-10-23 13:46:58 -0400

    feature: implement player spritesheet

---
+++ src/Viking/VikingGame.java
@@ -0,0 +1,32 @@
+package Viking;
+
+import Doctrina.Canvas;
+import Doctrina.Game;
+
+public class VikingGame extends Game {
+
+    private Player player;
+    private GamePad gamePad;
+    private World world;
+    @Override
+    protected void initialize() {
+        gamePad = new GamePad();
+        player = new Player(gamePad);
+        world = new World();
+        world.load();
+    }
+
+    @Override
+    protected void update() {
+        if (gamePad.isquietPressed()){
+            stop();
+        }
+        player.update();
+    }
+
+    @Override
+    protected void draw(Canvas canvas) {
+        world.draw(canvas);
+        player.draw(canvas);
+    }
+}
