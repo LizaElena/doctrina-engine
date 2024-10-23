3e07b1761cf653714aea9435527b6431a8d4343c
LizaElena <2234743@cegepst.qc.ca>
2024-10-23 15:11:53 -0400

    feature: implement tree and world

--- src/Viking/GamePad.java
+++ src/Viking/GamePad.java
@@ -6,11 +6,19 @@ import java.awt.event.KeyEvent;
 
 public class GamePad extends MouvementController {
     private int quitKey = KeyEvent.VK_Q;
-
+    private final int fireKey = KeyEvent.VK_SPACE;
     public GamePad(){
         bindKey(quitKey);
+        bindKey(fireKey);
     }
     public boolean isquietPressed(){
         return isKeyPressed(quitKey);
+    }
+
+    public boolean isQuitPressed(){
+        return isKeyPressed(quitKey);
+    }
+    public boolean isFirePressed(){
+        return isKeyPressed(fireKey);
     }
 }
--- src/Viking/VikingGame.java
+++ src/Viking/VikingGame.java
@@ -3,17 +3,37 @@ package Viking;
 import Doctrina.Canvas;
 import Doctrina.Game;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 public class VikingGame extends Game {
 
     private Player player;
     private GamePad gamePad;
     private World world;
+    private Tree tree;
+
+
+    private int soundCoolDown = 0;
     @Override
     protected void initialize() {
         gamePad = new GamePad();
         player = new Player(gamePad);
         world = new World();
+        tree = new Tree();
         world.load();
+        tree.load();
+        try {
+            Clip clip = AudioSystem.getClip();
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(getClass().getClassLoader().getResourceAsStream("audios/music.wav"));
+
+            clip.open(audioInputStream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            clip.start();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -22,11 +42,33 @@ public class VikingGame extends Game {
             stop();
         }
         player.update();
+        soundCoolDown--;
+        if (gamePad.isFirePressed() && soundCoolDown <= 0){
+            soundCoolDown = 100;
+
+            try {
+                Clip clip = AudioSystem.getClip();
+                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(getClass().getClassLoader().getResourceAsStream("audios/fire.wav"));
+                clip.open(audioInputStream);
+                clip.start();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     protected void draw(Canvas canvas) {
         world.draw(canvas);
-        player.draw(canvas);
+        //tree.draw(canvas);
+        //player.draw(canvas);
+        // 80 - 28 (max layer) = 52
+        if (player.getY() < tree.getY() + 52){
+            player.draw(canvas);
+            tree.draw(canvas);
+        }else {
+            tree.draw(canvas);
+            player.draw(canvas);
+        }
     }
 }
